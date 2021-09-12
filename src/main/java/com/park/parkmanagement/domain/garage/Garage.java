6eab107cca71279ac004b10282fb1b97f1a774ac
ahmetbbyk <ahmet-biyik@hotmail.com>
2021-09-13 00:57:18 +0300

    ddd

--- src/main/java/com/park/parkmanagement/domain/garage/Garage.java
+++ src/main/java/com/park/parkmanagement/domain/garage/Garage.java
@@ -1,8 +1,6 @@
 package com.park.parkmanagement.domain.garage;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.park.parkmanagement.domain.vehicle.Vehicle;
@@ -11,80 +9,81 @@ import com.park.parkmanagement.exception.ParkingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 public class Garage {
 	private static final Logger logger = LoggerFactory.getLogger(Garage.class);
-    private final ParkingSpot[] spots;
-    private int availableSpots;
+	private final ParkingSpot[] spots;
+	private int availableSpots;
 
-    public Garage(int numberOfSpots) {
-        availableSpots = numberOfSpots;
-        spots = new ParkingSpot[numberOfSpots];
+	public Garage(int numberOfSpots) {
+		availableSpots = numberOfSpots;
+		spots = new ParkingSpot[numberOfSpots];
 
-        for (var i = 0; i < numberOfSpots; i++) {
-            spots[i] = new ParkingSpot();
-        }
-    }
+		for (var i = 0; i < numberOfSpots; i++) {
+			spots[i] = new ParkingSpot();
+		}
+	}
 
-    public int availableSpots() {
-        return availableSpots;
-    }
+	public int availableSpots() {
+		return availableSpots;
+	}
 
-    public boolean parkVehicle(Vehicle vehicle) throws ParkingException {
-        if (availableSpots() < vehicle.getSpotsNeeded())
-            return false;
+	public boolean parkVehicle(Vehicle vehicle) throws ParkingException {
+		if (availableSpots() < vehicle.getSpotsNeeded())
+			return false;
 
-        int spotNumber = findAvailableSpots(vehicle);
-        if (spotNumber < 0)
-            return false;
+		int spotNumber = findAvailableSpots(vehicle);
+		if (spotNumber < 0)
+			return false;
 
-        if(vehicle.isExist(spots))
-    		throw new ParkingException(ExceptionCode.VEHICLE_ALLREADY_EXIST.getMessage(), ExceptionCode.VEHICLE_ALLREADY_EXIST);
-     
-        return parkStartingAtSpot(spotNumber, vehicle);
-    }
+		if (vehicle.isExist(spots))
+			throw new ParkingException(ExceptionCode.VEHICLE_ALLREADY_EXIST.getMessage(),
+					ExceptionCode.VEHICLE_ALLREADY_EXIST);
 
-    private boolean parkStartingAtSpot(int number, Vehicle vhcl) {
-        vhcl.clearSpots();
-        var success = true;
-        for (int i = number; i < number + vhcl.getSpotsNeeded(); i++) {
-        	// full slots with vehicle
-            success &= spots[i].park(i, vhcl);
-            logger.info("Allocating the spot.. " + i + " for vehicle :" + vhcl.vehiclePlate +" - "+ vhcl.getVehicleType());
-        }
-        availableSpots -= vhcl.getSpotsNeeded();
-        return success;
-    }
+		return parkStartingAtSpot(spotNumber, vehicle);
+	}
 
-    private int findAvailableSpots(Vehicle vehicle) {
-        for (var i = 0; i < spots.length; i++) {
-            if (spots[i].isAvailable() && vehicle.canFitInSpot(i, spots)) {
-                return i;
-            }
-        }
-        return -1;
-    }
+	private boolean parkStartingAtSpot(int number, Vehicle vhcl) {
+		vhcl.clearSpots();
+		var success = true;
+		for (int i = number; i < number + vhcl.getSpotsNeeded(); i++) {
+			// full slots with vehicle
+			success &= spots[i].park(i, vhcl);
+			logger.info("Allocating the spot.. " + i + " for vehicle :" + vhcl.vehiclePlate + " - "
+					+ vhcl.getVehicleType());
+		}
+		availableSpots -= vhcl.getSpotsNeeded();
+		return success;
+	}
 
-    public void spotFreed(Vehicle vehicle) {
-        for (ParkingSpot spot : spots) {
-            if (spot.getVehicle() != null && spot.getVehicle().getVehiclePlate().equals(vehicle.getVehiclePlate())) {
-                spot.removeVehicle();
-                availableSpots++;
-            }
-        }
+	private int findAvailableSpots(Vehicle vehicle) {
+		for (var i = 0; i < spots.length; i++) {
+			if (spots[i].isAvailable() && vehicle.canFitInSpot(i, spots)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
-        logger.info("Available Spots in the current level :" + availableSpots);
-    }
+	public void spotFreed(Vehicle vehicle) {
+		for (ParkingSpot spot : spots) {
+			if (spot.getVehicle() != null && spot.getVehicle().getVehiclePlate().equals(vehicle.getVehiclePlate())) {
+				spot.removeVehicle();
+				availableSpots++;
+			}
+		}
 
-    public String status() {
-    	
-        Map<String, List<ParkingSpot>> statusMap = Arrays.stream(spots).
-                collect(Collectors.groupingBy(o -> o.getVehicle().getVehiclePlate()));
-        return statusMap.keySet().stream()
-                .map(key -> key + " " + statusMap.get(key).stream().map(ParkingSpot::getSpotNumber).collect(Collectors.toList()))
-                .collect(Collectors.joining("\n"));
-    }
-    
-    public Vehicle findSpotbyId(int id) {
-    	return spots[id].getVehicle();
-    }
+		logger.info("Available Spots in the current level :" + availableSpots);
+	}
+
+	public String status() {
+		return Arrays.stream(spots)
+				.collect(Collectors.groupingBy(p -> p.getVehicle().getVehiclePlate(),
+						Collectors.mapping(ParkingSpot::getSpotNumber, Collectors.joining(",", "[", "]"))))
+				.entrySet().stream().map(e -> e.getKey() + " " + e.getValue()).collect(Collectors.joining("\n"));
+	}
+
+	public Vehicle findSpotbyId(int id) {
+		return spots[id].getVehicle();
+	}
 }
--- src/main/java/com/park/parkmanagement/domain/garage/ParkingSpot.java
+++ src/main/java/com/park/parkmanagement/domain/garage/ParkingSpot.java
@@ -3,31 +3,29 @@ package com.park.parkmanagement.domain.garage;
 import com.park.parkmanagement.domain.vehicle.Vehicle;
 
 public class ParkingSpot {
-    private Vehicle vehicle;
-    private int spotNumber;
+	private Vehicle vehicle;
+	private int spotNumber;
 
-    public boolean isAvailable() {
-        return vehicle == null;
-    }
+	public boolean isAvailable() {
+		return vehicle == null;
+	}
 
-    public boolean park(int spotNumber, Vehicle vhcl) {
-        this.spotNumber = spotNumber;
-        vehicle = vhcl;
-        vehicle.parkInSpot(this);
-        return true;
-    }
+	public boolean park(int spotNumber, Vehicle vhcl) {
+		this.spotNumber = spotNumber;
+		vehicle = vhcl;
+		vehicle.parkInSpot(this);
+		return true;
+	}
 
-    public Vehicle getVehicle() {
-        return vehicle;
-    }
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
 
+	public String getSpotNumber() {
+		return String.valueOf(spotNumber);
+	}
 
-    public int getSpotNumber() {
-        return spotNumber;
-    }
-
-
-    public void removeVehicle() {
-        vehicle = null;
-    }
+	public void removeVehicle() {
+		vehicle = null;
+	}
 }
